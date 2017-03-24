66828d9f2d3a9c4e4395a3b63c7e7c980170bb00
Jiemin Wang <jieminwang@Jiemins-MacBook-Air.local>
2017-03-23 23:10:25 -0400

    Initial commit: project setup

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://i.dailymail.co.uk/i/pix/2015/08/12/01/1AF9646D000005DC-3194442-image-a-21_1439339842820.jpg');
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
